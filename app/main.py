2a5b796a2b484970e5dd69b43760190a6779c1e6
ilivegod <sekajunior2014@gmail.com>
2026-03-18 00:12:01 +0100

    started upload document endpoint but did not finish

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from sqlalchemy import text
+from app.routers.documents import router as documents_router
 
 app = FastAPI()
+
+app.include_router(documents_router)
 
 
 @app.get("/health")
--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,33 +1,60 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, status, APIRouter
+from fastapi import FastAPI, UploadFile, File, HTTPException, status, APIRouter, Depends
 from pathlib import Path
-from app.main import app
+import uuid
+from app.models import document
+from app.models.document import Document_Status
+from app.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
 
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
 ALLOWED_EXTENSISONS = {".pdf" , ".docx"}
 MAX_FILE_SIZE = 10 * 1024 * 1024
 
+TEMP_USER_ID = uuid.uuid4()
+
+
 router = APIRouter()
 
+
 @router.post("/documents/upload")
-async def upload_document(file: UploadFile = File(...)):
+async def upload_document(file: UploadFile = File(...), db:AsyncSession = Depends(get_db)):
     #validating file type
     file_extension = Path(file.filename).suffix.lower()
     if file_extension not in ALLOWED_EXTENSISONS:
         raise HTTPException(status_code=400, detail = "Invalid file type. Only PDF/DOCX allowed.")
     
     #checking file size
-    file_size = await file.read()
+    file_content = await file.read()
+    file_size = len(file_content)
     if file_size > MAX_FILE_SIZE:
         raise HTTPException(status_code=400, detail="File too large.")
 
-    # saving in memeory and db
-    unique_filename = f"{uuid.uuid4()}{file_ext}"
+    # saving in memory 
+    unique_filename = f"{uuid.uuid4()}{file_extension}"
     file_path = UPLOAD_DIR / unique_filename
 
     try:
-        with file_path.open("wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+       file_path.open("wb").write(file_content)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Could not save file: {e}")
+
+
+    #saving in db
+
+    db_record = document(
+        user_id = TEMP_USER_ID,
+        file_name = file.filename,
+        file_type = file_extension,
+        file_path = str(file_path),
+        status = Document_Status.UPLOADED,
+    )
+    db.add(db_record)
+    await db.commit()
+    await db.refresh(db_record)
+
+    return {"id": str(db_record.id), "status": db_record.status}
+    
 
     
 
