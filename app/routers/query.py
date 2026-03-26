35cc0c812d0f3a24ec0244e39b7e021f2471ccfa
ilivegod <sekajunior2014@gmail.com>
2026-03-26 23:16:12 +0100

    made the retrieval function that checks close vectors to the user question and returns it.

---
+++ app/routers/query.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import get_db
+from app.services.retrieval import similarity_search
+from app.services.qa_chain import llm_prompt
+
+router = APIRouter()
+
+
+@router.post("/documents/query")
+async def query_document(question: str, db: AsyncSession = Depends(get_db)):
+    if not question:
+        return None
+    retrieved_text = similarity_search(question)
+    result = llm_prompt(retrieved_text)
+
+    return result
--- app/services/qa_chain.py
+++ app/services/qa_chain.py
@@ -6,13 +6,30 @@ from langchain_core.output_parsers import StrOutputParser
 def format_chunks_into_text(retrievedChunks: list):
     context_parts = []
     for i, chunk in enumerate(retrievedChunks, 1):
-        page_info = (
-            f" (Page {chunk.page_number})" if hasattr(chunk, "page_number") else ""
-        )
+        page_info = f" (Page {chunk.page_num})" if chunk.page_num else ""
 
         formatted_chunk = f"[{i}]{page_info}: {chunk.content.strip()}"
         context_parts.append(formatted_chunk)
     return "\n\n".join(context_parts)
 
 
-# def llm_prompt(question: str, retrievedChunks: list):
+def llm_prompt(question: str, retrievedChunks: list):
+    formatted_text = format_chunks_into_text(retrievedChunks)
+    prompt = PromptTemplate.from_template(
+        """Answer the question based only on the following context.
+        Cite your sources using the numbers in brackets.
+        If the context doesn't contain the answer, say "I don't have enough information to answer this."
+
+        Context:{context}
+
+        Question:{question}
+        """
+    )
+
+    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+
+    chain = prompt | model | StrOutputParser()
+
+    result = chain.invoke({"context": formatted_text, "question": question})
+
+    return result
